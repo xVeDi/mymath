88fecf39fbdefe7708c38d51771b9e90d889be47
xVeDi <xvedi@yandex.ru>
2022-08-05 12:56:12 +0300

    v0.2

--- addition.go
+++ addition.go
@@ -3,3 +3,17 @@ package mymath
 func Add(a, b int) int {
 	return a + b
 }
+
+func AddArr(a, b []int) []int {
+	length := len(a)
+	if length-len(b) > 0 {
+		length = len(b)
+	}
+	c := make([]int, length)
+
+	for i := 0; i < length; i++ {
+		c[i] = a[i] + b[i]
+	}
+
+	return c
+}
