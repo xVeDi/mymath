20c3de1bc4c55a2310c90de2d4bc2036e7cfbcb1
xVeDi <xvedi@yandex.ru>
2022-08-05 16:16:09 +0300

    v0.3

--- addition.go
+++ addition.go
@@ -1,18 +1,29 @@
 package mymath
 
+// Add 2 int values
 func Add(a, b int) int {
 	return a + b
 }
 
+// AddArr Add 2 int slices
 func AddArr(a, b []int) []int {
 	length := len(a)
-	if length-len(b) > 0 {
+	if length-len(b) < 0 {
 		length = len(b)
 	}
 	c := make([]int, length)
 
 	for i := 0; i < length; i++ {
-		c[i] = a[i] + b[i]
+		aa := a[i]
+		//if ok != nil {
+		//	aa = 0
+		//}
+		bb := b[i]
+		//if ok != nil {
+		//	bb = 0
+		//}
+
+		c[i] = aa + bb
 	}
 
 	return c
